e3b00bacc812b00c9f12192225acc7d61b0d5d44
Jordan Hewitt <jordan.hewitta7@gmail.com>
2019-10-18 11:28:24 -0400

    Finished objective 2

--- info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
+++ info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
@@ -1,10 +1,39 @@
+var state =[""];
+var counter =0;
+
 function boardSetup(){
-    
+
     var board = document.getElementById("board")
     var boardSections = board.getElementsByTagName("div");
 
     for(i=0; i<boardSections.length;i++){
         boardSections[i].className = "square";
+    }
+
+}
+
+
+function makePlay(e){
+
+    if(e.target.nodeName == "DIV"){
+
+        if(state[0] == ""){
+            state[0] = "X";
+            e.target.innerHTML = "X";
+            e.target.classList.add("X");
+
+        }else if(state[counter] == "O"){
+            state.push("X");
+            counter--;
+            e.target.innerHTML = "X";
+            e.target.classList.add("X");
+
+        }else if(state[counter] == "X"){
+            state.push("O");
+            counter++;
+            e.target.innerHTML = "O";
+            e.target.classList.add("O");
+        }
     }
 }
 
@@ -12,6 +41,7 @@ function boardSetup(){
 window.onload = function(){
     
     boardSetup();
-
+    console.log(board);
+    board.addEventListener("click",makePlay);
 }
 
